1ff8f35a6f76721c23c24ead0cad7a25e8ca832c
Lauryn Davis and Riley Starnes <riley@>
2016-01-04 13:12:20 -0800

    add 3rd spec

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -8,4 +8,7 @@ describe('String#title_case') do
   it("capitalizes the first letter of a multiple word string") do
     expect(("beowulf begins").title_case()).to(eq("Beowulf Begins"))
   end
+  it("does NOT capitalize designated words") do
+    expect(("beowolf from brighton beach").title_case()).to(eq("Beowulf from Brighton Beach"))
+  end 
 end
