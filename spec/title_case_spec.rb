cb289a1138a383a2b33966044582f7d3f0d785a9
Lauryn Davis and Riley Starnes <riley@>
2016-01-04 11:55:27 -0800

    pass first spec

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,5 +2,7 @@ require('rspec')
 require('title_case')
 
 describe('String#title_case') do
-
-end 
+  it("capitalizes the first letter of a word") do
+    expect(("beowulf").title_case()).to(eq("Beowulf"))
+  end
+end
